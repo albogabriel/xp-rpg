32c40aaaf493bc8f968df07dc41cf39e49aa74d8
Gabriel Albo Trindade <gabriel@albo.com.br>
2010-10-21 02:07:45 -0200

    UI adjustments on iterations labels

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,7 @@
 class Story < ActiveRecord::Base
 	belongs_to :iteration
+	
+		validates :title, :presence => true
+		validates :description, :presence => true,
+		                        :length => { :minimum => 5 }
 end
