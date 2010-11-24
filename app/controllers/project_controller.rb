0db94d4c3d47516153ced702b483152b0fc12667
Gabriel Albo Trindade <gabriel@albo.com.br>
2010-11-24 15:03:08 -0200

    Added reset method to run sql from the main view.

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -17,4 +17,8 @@ class ProjectController < ApplicationController
   def about
   end
 
+  def reset
+   ActiveRecord::Base.connection.execute('INSERT INTO "stories" VALUES ("40","TESTE INSERTION","As a NASA scientist I would like to have a food processing unit so I can produce food out of small protein pills.","3","Ready for Dev","2010-10-11 20:10:13.452822","2010-11-22 04:50:04.501580","1","M");')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 App01::Application.routes.draw do
   
+  resources :projects
+
   resources :requirements
 
   resources :iterations do
@@ -75,5 +77,5 @@ App01::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+   match ':controller(/:action(/:id(.:format)))'
 end
